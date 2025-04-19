b7a5c8f1b9ff43cdc582a0c933d12fd488ea84ff
decce6 <decce6@proton.me>
2025-04-19 08:34:25 +0800

    Speedup Xaero's Minimap method invoking by replacing method handles with direct method calls

--- src/main/java/me/decce/gnetum/compat/xaerominimap/XaeroMinimapCompat.java
+++ src/main/java/me/decce/gnetum/compat/xaerominimap/XaeroMinimapCompat.java
@@ -2,34 +2,18 @@ package me.decce.gnetum.compat.xaerominimap;
 
 import me.decce.gnetum.Gnetum;
 import net.minecraftforge.fml.common.Loader;
-
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
+import xaero.common.core.XaeroMinimapCore;
 
 // Xaero's Minimap renders the ingame waypoints in a very werid way (ASM is used to insert the call into
 // GuiIngameForge.renderGameOverlay, which broke due to our mixin), so we manually call their render method as a
 // workaround.
 // This is only for the ingame waypoint rendering. Minimap rendering is not relevant.
 public class XaeroMinimapCompat {
-    public static boolean modInstalled;
-    private static MethodHandle methodHandle;
-    static {
-        if (Loader.isModLoaded("xaerominimap")) {
-            try {
-                Class<?> clazzXaeroMinimapCore = Class.forName("xaero.common.core.XaeroMinimapCore");
-                MethodHandles.Lookup publicLookup = MethodHandles.publicLookup();
-                MethodType methodType = MethodType.methodType(void.class, float.class);
-                methodHandle = publicLookup.findStatic(clazzXaeroMinimapCore, "beforeIngameGuiRender", methodType);
-                modInstalled = true;
-            }
-            catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException ignored) {}
-        }
-    }
+    public static boolean modInstalled = Loader.isModLoaded("xaerominimap");
 
     public static void callBeforeIngameGuiRender(float partialTicks) {
         try {
-            methodHandle.invokeExact(partialTicks);
+            XaeroMinimapCore.beforeIngameGuiRender(partialTicks);
         } catch (Throwable e) {
             Gnetum.LOGGER.error("Error invoking Xaero's Minimap method!", e);
             modInstalled = false;
