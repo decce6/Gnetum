72663c49bbb853dba3f8ec4c336e12856ff1d3a8
decce6 <decce6@proton.me>
2025-04-19 08:30:38 +0800

    Speedup OptiFine config access by replacing method handles with mixin invokers

--- src/main/java/me/decce/gnetum/compat/optifine/OptiFineCompat.java
+++ src/main/java/me/decce/gnetum/compat/optifine/OptiFineCompat.java
@@ -1,34 +1,16 @@
 package me.decce.gnetum.compat.optifine;
 
 import me.decce.gnetum.Gnetum;
+import me.decce.gnetum.mixins.compat.optifine.ConfigAccessor;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.client.FMLClientHandler;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
-
 public class OptiFineCompat {
-    public static boolean optifineInstalled;
-
-    private static MethodHandle methodHandle;
-
-    static {
-        if (FMLClientHandler.instance().hasOptifine()) {
-            try {
-                Class<?> clazzConfig = Class.forName("Config");
-                MethodHandles.Lookup publicLookup = MethodHandles.publicLookup();
-                MethodType methodType = MethodType.methodType(boolean.class);
-                methodHandle = publicLookup.findStatic(clazzConfig, "isVignetteEnabled", methodType);
-                optifineInstalled = true;
-            }
-            catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException ignored) {}
-        }
-    }
+    public static boolean optifineInstalled = FMLClientHandler.instance().hasOptifine();
 
     public static boolean isVignetteEnabled() {
         try {
-            return (boolean)methodHandle.invokeExact();
+            return ConfigAccessor.invokeIsVignetteEnabled();
         } catch (Throwable e) {
             Gnetum.LOGGER.error("Error invoking OptiFine method when retrieving vignette status.", e);
             optifineInstalled = false;
---
+++ src/main/java/me/decce/gnetum/mixins/compat/optifine/ConfigAccessor.java
@@ -0,0 +1,14 @@
+package me.decce.gnetum.mixins.compat.optifine;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Pseudo
+@Mixin(targets = "Config")
+public interface ConfigAccessor {
+    @Invoker("isVignetteEnabled")
+    static boolean invokeIsVignetteEnabled() {
+        throw new AssertionError();
+    }
+}
