d8fc465e0c4e4192a32b4bd339c5a2219ad6e4d4
decce6 <decce6@proton.me>
2025-12-28 09:41:00 +0800

    Tweak default pass setting

--- src/main/java/me/decce/gnetum/SuggestedPass.java
+++ src/main/java/me/decce/gnetum/SuggestedPass.java
@@ -4,8 +4,8 @@ public class SuggestedPass {
     public static int get(String name) {
         int pass = switch (name) {
             case Gnetum.HAND_ELEMENT, "minecraft:vignette", "minecraft:helmet", "minecraft:portal" -> 1;
-            case "minecraft:hotbar", "minecraft:crosshairs", "minecraft:boss_health", "gnetum:player_left", "gnetum:player_right", "minecraft:sleep_overlay", "minecraft:jump_meter", "minecraft:experience", "minecraft:tool_highlight", "minecraft:hud_text" -> 2;
-            case "minecraft:fps_graph", "minecraft:potion_icons", "minecraft:record_overlay", "minecraft:subtitles", "minecraft:title", "minecraft:scoreboard", "minecraft:chat_panel", "minecraft:player_list" -> 3;
+            case "minecraft:hotbar", "minecraft:crosshairs", "minecraft:boss_health", "gnetum:player_left", "gnetum:player_right", "minecraft:sleep_overlay", "minecraft:jump_meter", "minecraft:experience", "minecraft:tool_highlight" -> 2;
+            case "minecraft:hud_text", "minecraft:fps_graph", "minecraft:potion_icons", "minecraft:record_overlay", "minecraft:subtitles", "minecraft:title", "minecraft:scoreboard", "minecraft:chat_panel", "minecraft:player_list" -> 3;
             default -> 3;
         };
         if (pass > Gnetum.config.numberOfPasses) pass = Gnetum.config.numberOfPasses;
