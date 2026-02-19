dce1f48ab9009f153b8d5e11384c3babae68dfec
decce6 <decce6@proton.me>
2026-02-19 11:52:37 +0800

    Apply Jade bossbar fix on all platforms

--- src/main/java/me/decce/gnetum/mixins/compat/jade/ClientProxyMixin.java
+++ src/main/java/me/decce/gnetum/mixins/compat/jade/ClientProxyMixin.java
@@ -5,15 +5,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import snownee.jade.util.ClientProxy;
 
 @Pseudo
-@Mixin(value = ClientProxy.class, remap = false)
+@Mixin(targets = "snownee.jade.util.ClientProxy", remap = false)
 public class ClientProxyMixin {
-	//? neoforge {
-	/*@Redirect(method = "getBossBarRect", at = @At(value = "FIELD", target = "Lsnownee/jade/util/ClientProxy;bossbarShown:Z"))
+	@Redirect(method = "getBossBarRect", at = @At(value = "FIELD", target = "Lsnownee/jade/util/ClientProxy;bossbarShown:Z"))
 	private static boolean getBossBarRect() {
 		return JadeCompat.bossBarShown;
 	}
-	*///?}
 }
