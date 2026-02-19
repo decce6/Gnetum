fed2a55cec27ed89b254f04fb6c34eccf64c61a2
decce6 <decce6@proton.me>
2026-02-19 12:38:03 +0800

    Fix mixin error on 26.1

--- src/main/java/me/decce/gnetum/mixins/fabric/HudRenderCallbackMixin.java
+++ src/main/java/me/decce/gnetum/mixins/fabric/HudRenderCallbackMixin.java
@@ -13,7 +13,11 @@ import org.spongepowered.asm.mixin.injection.At;
 @SuppressWarnings("deprecation")
 @Mixin(value = HudRenderCallback.class, remap = false)
 public interface HudRenderCallbackMixin {
+	//? >26 {
+	/*@WrapOperation(method = "lambda$static$1", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/api/client/rendering/v1/HudRenderCallback;onHudRender(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/client/DeltaTracker;)V"))
+	 *///? } else {
 	@WrapOperation(method = "lambda$static$0", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/api/client/rendering/v1/HudRenderCallback;onHudRender(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/client/DeltaTracker;)V"))
+	//? }
 	private static void gnetum$render(HudRenderCallback callback, GuiGraphics guiGraphics, DeltaTracker deltaTracker, Operation<Void> original) {
 		if (!Gnetum.config.isEnabled() || !Gnetum.rendering) {
 			original.call(callback, guiGraphics, deltaTracker);
