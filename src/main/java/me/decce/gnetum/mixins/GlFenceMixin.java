d6d443659e3298df648ab4fa4597cb549e454157
decce6 <decce6@proton.me>
2026-02-20 15:59:34 +0800

    Fix enabled config

--- src/main/java/me/decce/gnetum/mixins/GlFenceMixin.java
+++ src/main/java/me/decce/gnetum/mixins/GlFenceMixin.java
@@ -2,6 +2,7 @@ package me.decce.gnetum.mixins;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.mojang.blaze3d.opengl.GlFence;
+import me.decce.gnetum.Gnetum;
 import org.lwjgl.opengl.GL32C;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,9 +11,12 @@ import org.spongepowered.asm.mixin.injection.At;
 public class GlFenceMixin {
     @ModifyExpressionValue(method = "awaitCompletion", at = @At(value = "CONSTANT", args = "intValue=0"))
     private int gnetum$modifyClientWaitFlags(int original) {
-        // Only flush commands up to the point where the sync object is placed, instead of where the wait command is
-        // issued (for OpenGL 4.5+)
-        // See: https://wikis.khronos.org/opengl/Sync_Object#Flushing_and_contexts
-        return GL32C.GL_SYNC_FLUSH_COMMANDS_BIT;
+        if (Gnetum.config.isEnabled()) {
+            // Only flush commands up to the point where the sync object is placed, instead of where the wait command is
+            // issued (for OpenGL 4.5+)
+            // See: https://wikis.khronos.org/opengl/Sync_Object#Flushing_and_contexts
+            return GL32C.GL_SYNC_FLUSH_COMMANDS_BIT;
+        }
+        return original;
     }
 }
