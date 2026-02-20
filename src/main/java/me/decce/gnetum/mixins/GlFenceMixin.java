d73da83a36ecc16e442f0e01b1b525cb01dd543d
decce6 <decce6@proton.me>
2026-02-20 15:55:52 +0800

    Optimize buffer synchronization

---
+++ src/main/java/me/decce/gnetum/mixins/GlFenceMixin.java
@@ -0,0 +1,18 @@
+package me.decce.gnetum.mixins;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.mojang.blaze3d.opengl.GlFence;
+import org.lwjgl.opengl.GL32C;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(GlFence.class)
+public class GlFenceMixin {
+    @ModifyExpressionValue(method = "awaitCompletion", at = @At(value = "CONSTANT", args = "intValue=0"))
+    private int gnetum$modifyClientWaitFlags(int original) {
+        // Only flush commands up to the point where the sync object is placed, instead of where the wait command is
+        // issued (for OpenGL 4.5+)
+        // See: https://wikis.khronos.org/opengl/Sync_Object#Flushing_and_contexts
+        return GL32C.GL_SYNC_FLUSH_COMMANDS_BIT;
+    }
+}
--- src/main/java/me/decce/gnetum/mixins/MappableRingBufferMixin.java
+++ src/main/java/me/decce/gnetum/mixins/MappableRingBufferMixin.java
@@ -1,16 +1,31 @@
 package me.decce.gnetum.mixins;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import me.decce.gnetum.Constants;
+import me.decce.gnetum.Gnetum;
 import net.minecraft.client.renderer.MappableRingBuffer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+// We require additional buffer flushing before switching framebuffers. To avoid client side stall when awaiting
+// fence completion on glClientWaitSync, we increase the buffer count here.
 @Mixin(MappableRingBuffer.class)
 public class MappableRingBufferMixin {
-    // We require additional buffer flushing before switching framebuffers. Thus, we double the buffer count to avoid
-    // pipeline stall.
-    @ModifyExpressionValue(method = { "<init>", "rotate", "close" } , at = @At(value = "CONSTANT", args = "intValue=3"))
+    @ModifyExpressionValue(method = { "<init>", "close" } , at = @At(value = "CONSTANT", args = "intValue=3"))
     private int gnetum$increaseBufferSize(int original) {
-        return original * 2;
+        // We must generate a size that is the largest we might need because we cannot resize afterward
+        return original * 4;
+    }
+
+    @ModifyExpressionValue(method = "rotate" , at = @At(value = "CONSTANT", args = "intValue=3"))
+    private int gnetum$rotate$increaseBufferSize(int original) {
+        if (!Gnetum.config.isEnabled()) {
+            return original;
+        }
+        if (!Gnetum.getElement(Constants.DEBUG_OVERLAY).isUncached()) {
+            // F3 caching requires two additional flushes
+            return original * 4;
+        }
+        return original * 3;
     }
 }
